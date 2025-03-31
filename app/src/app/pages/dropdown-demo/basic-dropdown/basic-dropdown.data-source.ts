e68183ec7d2b8d1cbd94c21d974ee662a2a43b5b
Hair, Daniel <danielmhair@gmail.com>
2025-03-31 12:59:19 -0600

    fix: dropdowns with infinite scrolling

--- app/src/app/pages/dropdown-demo/basic-dropdown/basic-dropdown.data-source.ts
+++ app/src/app/pages/dropdown-demo/basic-dropdown/basic-dropdown.data-source.ts
@@ -1,5 +1,6 @@
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
+import { PageableResult } from "@devcrate/ngx-dc-data-sources";
 import { NgxDcDropdownApiDataSource } from "@devcrate/ngx-dc-dropdown";
 
 export interface BasicModel {
@@ -23,6 +24,36 @@ export class BasicDropdownDataSource extends NgxDcDropdownApiDataSource<BasicMod
   constructor() {
     const httpClient = inject(HttpClient);
 
+    super(httpClient)
+  }
+}
+
+
+export interface MyModel {
+  id: number
+}
+
+@Injectable()
+export class InfiniteDropdownDataSource extends NgxDcDropdownApiDataSource<MyModel> {
+  public relativePath = "https://devcrate.onrender.com/api/people"
+  public rowHeight: number = 80
+  public pageSize = 10
+  public maxTotalCount = 1000
+
+  public isActiveItem(selectedItem: MyModel, currentItem: MyModel): boolean {
+    return selectedItem.id == currentItem.id
+  }
+
+  public async retrieveDataItems(overrideParams: any = null): Promise<PageableResult<MyModel>> {
+    return super.retrieveDataItems(overrideParams);
+  }
+
+  public trackItems(item: MyModel) {
+    return item.id
+  }
+
+  constructor() {
+    const httpClient = inject(HttpClient);
     super(httpClient)
   }
 }
